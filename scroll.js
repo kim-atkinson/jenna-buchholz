7e1717a7f550552067070c7bbf03cf9c6453c0c0
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-26 15:27:52 -0400

    Added client and page attributes.

--- scroll.js
+++ scroll.js
@@ -1,6 +1,10 @@
 const pixelsTag = document.querySelector("div.pixels")
 const bodyTag = document.querySelector("body")
 const progressTag = document.querySelector("div.progress")
+const sections = document.querySelectorAll("section")
+const clientTag = document.querySelector("div.client")
+const pageTag = document.querySelector("div.page")
+
 
 
 // When user scrolls, update the pixels tag
@@ -19,5 +23,21 @@ document.addEventListener("scroll", function () {
     const totalScrollableDistance = pageHeight - window.innerHeight
 
     const percentage = pixels / totalScrollableDistance
+
     progressTag.style.width = `${100 * percentage}%`
+})
+
+// When user scrolls page, see how far down the page user scrolls
+// Then for each section, checked whether passed and if haved...
+// Update the text in the header
+document.addEventListener("scroll", function () {
+    const pixels = window.pageYOffset
+
+
+    sections.forEach(section => {
+        if (section.offsetTop - 100 <= pixels) {
+            clientTag.innerHTML = section.getAttribute("data-client")
+            pageTag.innerHTML =  section.getAttribute("data-page")
+        }
+    })
 })
