e86b00ce6840c820714154ac1a235cab61f1986e
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-26 10:23:30 -0400

    Progress bar added to the top of site.

--- scroll.js
+++ scroll.js
@@ -1,9 +1,23 @@
 const pixelsTag = document.querySelector("div.pixels")
+const bodyTag = document.querySelector("body")
+const progressTag = document.querySelector("div.progress")
+
 
 // When user scrolls, update the pixels tag
 document.addEventListener("scroll", function() {
 
     const pixels = window.pageYOffset
 
-    pixelsTag.innerHTML = pixels + 
+    pixelsTag.innerHTML = pixels
+})
+
+// When user scrolls the page, progress bar will keep track of the distance
+// Review video 8 for explanation
+document.addEventListener("scroll", function () {
+    const pixels = window.pageYOffset
+    const pageHeight = bodyTag.getBoundingClientRect().height
+    const totalScrollableDistance = pageHeight - window.innerHeight
+
+    const percentage = pixels / totalScrollableDistance
+    progressTag.style.width = `${100 * percentage}%`
 })
